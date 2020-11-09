62f8729b4b0533d8f62ca1169c3e8f61197e5ec8
Haaris Ahmed <haaris710@live.com>
2020-11-09 15:14:02 -0500

    Run simulation for various A values

--- main.c
+++ main.c
@@ -14,13 +14,46 @@
 int main(void)
 {
     double simTime = 1000.0;
-    double A = 5.0;
+    double A = 7.0;
     double L = 1500.0;
     double R = 1000000.0;
     double D = 10.0;
     double S = (2.0/3.0)*3.0*100000000.0;
-
     double timeStamp = 0;
+
+    printf("SIMULATION A = %f ================================\r\n", A);
+    for (double N = 20; N < 120; N += 20)
+    {
+        app_simulator_init(simTime, A, L, R, N, D, S);
+
+        while(timeStamp <= simTime)
+        {
+            timeStamp =  app_simulator_run();
+        }
+
+        app_simulator_print_results();
+        app_simulator_deinit();
+        timeStamp = 0;
+    }
+
+    A = 10;
+    printf("SIMULATION A = %f ================================\r\n", A);
+    for (double N = 20; N < 120; N += 20)
+    {
+        app_simulator_init(simTime, A, L, R, N, D, S);
+
+        while(timeStamp <= simTime)
+        {
+            timeStamp =  app_simulator_run();
+        }
+
+        app_simulator_print_results();
+        app_simulator_deinit();
+        timeStamp = 0;
+    }
+
+    A = 20;
+    printf("SIMULATION A = %f ================================\r\n", A);
     for (double N = 20; N < 120; N += 20)
     {
         app_simulator_init(simTime, A, L, R, N, D, S);
